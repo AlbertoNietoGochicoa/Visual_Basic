8783f677f80a9c18e83cfc9749a631ef72d32306
Alberto Nieto <luptupulus@hotmail.com>
2015-11-03 20:08:31 +0100

    03/11/2015

--- EjercicioIO3formas/EjercicioIO3formas/Module1.vb
+++ EjercicioIO3formas/EjercicioIO3formas/Module1.vb
@@ -1,7 +1,33 @@
-﻿Module Module1
+﻿Imports System.IO
+Imports System.Text
+
+Module Module1
+
+    Dim path As String = "D:\Desarollo de sistemas\GITHUB\Visual_Basic\EjercicioIO3formas\"
+
+    Structure persona
+        Dim nombre As String
+        Dim direccion As String
+        Dim telefono As Double
+        Dim mail As String
+    End Structure
+
+    Sub pedirPath()
+        Console.WriteLine("Escribe el nombre del archivo donde quieres guardar la informacion: ")
+        Dim archivo As String = Console.ReadLine
+        path = path + archivo
+    End Sub
+
+
+
 
     Sub Main()
+        'abrimos un flujo
 
+
+
+
+        System.Threading.Thread.Sleep(1500)
     End Sub
 
 End Module
