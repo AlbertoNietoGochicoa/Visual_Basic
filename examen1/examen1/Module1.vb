8521c62aafa17054de71feda910d7a66697d0b4b
Alberto Nieto <luptupulus@hotmail.com>
2015-11-11 21:26:02 +0100

    cambios

--- examen1/examen1/Module1.vb
+++ examen1/examen1/Module1.vb
@@ -7,39 +7,40 @@
     Dim rnd As Random = New Random()
 
 
-    Dim array() As Integer
+    Dim arrayRandom() As Integer
 
     Sub entradaDatoConAjusteArray()
         'Solicitamos un numero y ajustamos el array a es numero.
-        Console.WriteLine("Introduce un numero para ajustar el array:")
-        leerEntero()
-        ReDim array(num)
+        Do
+            Console.Clear()
+            Console.WriteLine("Introduce un numero para ajustar el array:")
+        Loop While (leerEntero())
+
+        ReDim arrayRandom(num)
     End Sub
 
     Sub rellenarArray()
         'Se rellena el array con valores aleatorios entre el 0 y el 500
-        For i = 0 To (num - 1)
-            array(i) = rnd.Next(0, 501)
+        For i = 0 To (num)
+            arrayRandom(i) = rnd.Next(0, 501)
         Next
 
     End Sub
 
     Sub imprimirArray()
         For i = 0 To num
-            Console.WriteLine("Numero:-> " & array(i))
+            Console.WriteLine("Numero:-> " & arrayRandom(i))
             System.Threading.Thread.Sleep(300)
         Next
 
-        Console.WriteLine(vbCrLf & "Ahora del reves")
+        Console.WriteLine(vbCrLf & "Ahora del reves" & vbCrLf)
 
         'mirar como se puede hacer que esto de abajo funcione!!
 
-        array.Reverse(array)
-
-
+        Array.Reverse(arrayRandom)
 
         For i = 0 To num
-            Console.WriteLine("Numero:-> " & array(i))
+            Console.WriteLine("Numero:-> " & arrayRandom(i))
             System.Threading.Thread.Sleep(300)
         Next
 
@@ -48,36 +49,38 @@
 
     Sub imprimirMaxMin()
 
-        Console.WriteLine(vbCrLf & " El numero maximo del array es el: " & array.Max)
-        Console.WriteLine(vbCrLf & " El numero maximo del array es el: " & array.Min)
+        Console.WriteLine(vbCrLf & " El numero maximo del array es el: " & arrayRandom.Max)
+        Console.WriteLine(vbCrLf & " El numero maximo del array es el: " & arrayRandom.Min)
 
 
     End Sub
 
-    Sub leerEntero()
-        Do
-            Try
+    Function leerEntero() As Boolean
+
+        Try
                 'recogemos el valor 
                 Console.Write(vbCrLf & "Numero: ")
                 num = Integer.Parse(Console.ReadLine) - 1
 
                 'validamos que el numero sea positivo
                 If (num <= 0) Then
-                    Console.WriteLine("ERROR, el numero introducido no es valido!!! intentalo de nuevo")
-                    cont = True
-                Else
+                Console.WriteLine("ERROR, el numero introducido no es valido!!! intentalo de nuevo")
+                System.Threading.Thread.Sleep(1000)
+                Return True
+            Else
                     'si es positivo continuamos
-                    cont = False
+                    Return False
                 End If
                 'si salta la excepcion el bucle continua
             Catch ex As Exception
                 Console.WriteLine("ERROR, ERROR, ERROR!!!!!  No has introducido un numero entero, intentalo de nuevo....")
-                cont = True
-            End Try
+            System.Threading.Thread.Sleep(1000)
+            Return True
+        End Try
 
 
-        Loop While (cont)
-    End Sub
+    End Function
+
     Sub Main()
 
         Console.WriteLine("")
@@ -90,6 +93,7 @@
 
         Console.WriteLine("pulsa una tecla para salir")
         Console.ReadKey()
+
     End Sub
 
 End Module
