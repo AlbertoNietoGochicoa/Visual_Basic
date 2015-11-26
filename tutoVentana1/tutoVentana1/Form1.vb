494d5bbdbb853d94eb60c7c0e5d3b9b1c1d6a6eb
Alberto Nieto <luptupulus@hotmail.com>
2015-11-26 18:44:21 +0100

    cambbios

---
+++ tutoVentana1/tutoVentana1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
+
+    Private Sub showButton_Click(sender As Object, e As EventArgs) Handles showButton.Click
+
+    End Sub
+End Class
