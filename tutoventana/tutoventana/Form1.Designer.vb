5a5ba0345fab9e31dc09c67fc82cc8a38f5669bd
Luptupulus <luptupulus@hotmail.com>
2015-12-02 11:01:27 +0100

    cambios

--- tutoventana/tutoventana/Form1.Designer.vb
+++ tutoventana/tutoventana/Form1.Designer.vb
@@ -22,17 +22,16 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.checkBox1 = New System.Windows.Forms.CheckBox()
-        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
-        Me.showButton = New System.Windows.Forms.Button()
-        Me.clearButton = New System.Windows.Forms.Button()
-        Me.backGroungButton = New System.Windows.Forms.Button()
-        Me.closeButton = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.backGroungButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.showButton = New System.Windows.Forms.Button()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.TableLayoutPanel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.FlowLayoutPanel1.SuspendLayout()
@@ -40,18 +39,21 @@ Partial Class Form1
         '
         'TableLayoutPanel1
         '
+        Me.TableLayoutPanel1.AllowDrop = True
+        Me.TableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.TableLayoutPanel1.ColumnCount = 2
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.checkBox1, 0, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.FlowLayoutPanel1, 1, 1)
+        Me.TableLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.[On]
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.95598!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.04403!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(555, 319)
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.5443!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.4557!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(899, 481)
         Me.TableLayoutPanel1.TabIndex = 0
         '
         'PictureBox1
@@ -59,10 +61,10 @@ Partial Class Form1
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.TableLayoutPanel1.SetColumnSpan(Me.PictureBox1, 2)
         Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(549, 226)
+        Me.PictureBox1.Size = New System.Drawing.Size(893, 395)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
@@ -70,81 +72,12 @@ Partial Class Form1
         '
         Me.checkBox1.AutoSize = True
         Me.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.checkBox1.Location = New System.Drawing.Point(3, 235)
+        Me.checkBox1.Location = New System.Drawing.Point(3, 404)
         Me.checkBox1.Name = "checkBox1"
-        Me.checkBox1.Size = New System.Drawing.Size(271, 81)
+        Me.checkBox1.Size = New System.Drawing.Size(443, 74)
         Me.checkBox1.TabIndex = 1
         Me.checkBox1.Text = "strech"
         Me.checkBox1.UseVisualStyleBackColor = True
-        '
-        'FlowLayoutPanel1
-        '
-        Me.FlowLayoutPanel1.Controls.Add(Me.showButton)
-        Me.FlowLayoutPanel1.Controls.Add(Me.clearButton)
-        Me.FlowLayoutPanel1.Controls.Add(Me.backGroungButton)
-        Me.FlowLayoutPanel1.Controls.Add(Me.closeButton)
-        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(280, 235)
-        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(272, 81)
-        Me.FlowLayoutPanel1.TabIndex = 2
-        '
-        'showButton
-        '
-        Me.showButton.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.showButton.AutoSize = True
-        Me.showButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.showButton.Location = New System.Drawing.Point(3, 3)
-        Me.showButton.Name = "showButton"
-        Me.showButton.Size = New System.Drawing.Size(110, 23)
-        Me.showButton.TabIndex = 0
-        Me.showButton.Text = "Mostrar una imagen"
-        Me.showButton.UseVisualStyleBackColor = True
-        '
-        'clearButton
-        '
-        Me.clearButton.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.clearButton.AutoSize = True
-        Me.clearButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.clearButton.Location = New System.Drawing.Point(3, 32)
-        Me.clearButton.Name = "clearButton"
-        Me.clearButton.Size = New System.Drawing.Size(110, 23)
-        Me.clearButton.TabIndex = 1
-        Me.clearButton.Text = "Borrar imagen"
-        Me.clearButton.UseVisualStyleBackColor = True
-        '
-        'backGroungButton
-        '
-        Me.backGroungButton.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.backGroungButton.AutoSize = True
-        Me.backGroungButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.backGroungButton.Location = New System.Drawing.Point(119, 3)
-        Me.backGroungButton.Name = "backGroungButton"
-        Me.backGroungButton.Size = New System.Drawing.Size(149, 23)
-        Me.backGroungButton.TabIndex = 2
-        Me.backGroungButton.Text = "Establecer el color de fondo"
-        Me.backGroungButton.UseVisualStyleBackColor = True
-        '
-        'closeButton
-        '
-        Me.closeButton.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.closeButton.AutoSize = True
-        Me.closeButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.closeButton.Location = New System.Drawing.Point(119, 32)
-        Me.closeButton.Name = "closeButton"
-        Me.closeButton.Size = New System.Drawing.Size(149, 23)
-        Me.closeButton.TabIndex = 3
-        Me.closeButton.Text = "cerrar"
-        Me.closeButton.UseVisualStyleBackColor = True
         '
         'OpenFileDialog1
         '
@@ -152,13 +85,83 @@ Partial Class Form1
         Me.OpenFileDialog1.Title = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" &
     "s (*.*)|*.*"
         '
+        'closeButton
+        '
+        Me.closeButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.closeButton.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.closeButton.Dock = System.Windows.Forms.DockStyle.Top
+        Me.closeButton.Location = New System.Drawing.Point(121, 32)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(149, 23)
+        Me.closeButton.TabIndex = 3
+        Me.closeButton.Text = "cerrar"
+        Me.closeButton.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.closeButton.UseVisualStyleBackColor = True
+        '
+        'backGroungButton
+        '
+        Me.backGroungButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.backGroungButton.AutoSize = True
+        Me.backGroungButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.backGroungButton.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.backGroungButton.Location = New System.Drawing.Point(121, 3)
+        Me.backGroungButton.Name = "backGroungButton"
+        Me.backGroungButton.Size = New System.Drawing.Size(149, 23)
+        Me.backGroungButton.TabIndex = 2
+        Me.backGroungButton.Text = "Establecer el color de fondo"
+        Me.backGroungButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.clearButton.AutoSize = True
+        Me.clearButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.clearButton.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.clearButton.Location = New System.Drawing.Point(18, 3)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(82, 23)
+        Me.clearButton.TabIndex = 1
+        Me.clearButton.Text = "Borrar imagen"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'showButton
+        '
+        Me.showButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.showButton.AutoSize = True
+        Me.showButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.showButton.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.showButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.showButton.Location = New System.Drawing.Point(3, 32)
+        Me.showButton.Name = "showButton"
+        Me.showButton.Size = New System.Drawing.Size(112, 25)
+        Me.showButton.TabIndex = 0
+        Me.showButton.Text = "Mostrar una imagen"
+        Me.showButton.UseVisualStyleBackColor = True
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoSize = True
+        Me.FlowLayoutPanel1.Controls.Add(Me.clearButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.showButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.backGroungButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.closeButton)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(452, 404)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(444, 74)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(549, 318)
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(899, 483)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.TableLayoutPanel1.PerformLayout()
@@ -172,11 +175,11 @@ Partial Class Form1
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents checkBox1 As CheckBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
     Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
     Friend WithEvents showButton As Button
     Friend WithEvents clearButton As Button
     Friend WithEvents backGroungButton As Button
     Friend WithEvents closeButton As Button
-    Friend WithEvents OpenFileDialog1 As OpenFileDialog
-    Friend WithEvents ColorDialog1 As ColorDialog
 End Class
--- tutoventana/tutoventana/Form1.vb
+++ tutoventana/tutoventana/Form1.vb
@@ -9,6 +9,7 @@
 
     Private Sub showButton_Click(sender As Object, e As EventArgs) Handles showButton.Click
 
+        'esto permite abrir la ventana de archivos
         If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
             PictureBox1.Load(OpenFileDialog1.FileName)
 
@@ -17,17 +18,29 @@
 
     Private Sub backGroungButton_Click(sender As Object, e As EventArgs) Handles backGroungButton.Click
 
+        'cambia el color de4 fondo
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.BackColor = ColorDialog1.Color
+        End If
+
     End Sub
 
     Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
-
+        'borra la imagen y la pone a null
+        PictureBox1.Image = Nothing
     End Sub
 
     Private Sub closeButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
+        'cerramos la ventana
+        Close()
 
     End Sub
 
     Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
 
     End Sub
 End Class
